1ce6db7879fa367725967a559008f87ea916b86d
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2023-06-27 20:38:17 +0300

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  const discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+    return arr; // нет корней
+  } else if (discriminant === 0) {
+    const root = -b / (2 * a);
+    return [root]; // один корень
+  } else {
+    const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [root1, root2]; // два корня
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
