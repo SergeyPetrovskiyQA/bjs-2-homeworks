6e779ac3a6be359d5d2d16341b1223cb2763a624
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2023-06-27 20:41:25 +0300

    решение

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,7 +4,7 @@ function solveEquation(a, b, c) {
   const discriminant = b ** 2 - 4 * a * c;
 
   if (discriminant < 0) {
-    return arr; // нет корней
+    return []; // нет корней
   } else if (discriminant === 0) {
     const root = -b / (2 * a);
     return [root]; // один корень
@@ -16,5 +16,25 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+	if (
+		typeof percent !== 'number' ||
+		typeof contribution !== 'number' ||
+		typeof amount !== 'number' ||
+		typeof countMonths !== 'number'
+	  ) {
+		return false;
+	  }
+	
+	  percent = percent / 100; // Преобразование процентной ставки в десятичное число
+	  const monthlyRate = percent / 12; // Месячная процентная ставка
+	  const creditAmount = amount - contribution; // Тело кредита
+	
+	  const monthlyPayment =
+		creditAmount *
+		(monthlyRate +
+		  monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)); // Ежемесячный платеж
+	
+	  const totalPayment = monthlyPayment * countMonths; // Общая сумма, которую придется заплатить
+	
+	  return +totalPayment.toFixed(2); // Округление до двух знаков после запятой и возврат результата
+	}
