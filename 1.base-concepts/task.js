e0145e6b3a598b637bb7c344f6e4cc2364274f4f
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2023-06-27 20:57:06 +0300

    решение 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,16 +1,16 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  const discriminant = b ** 2 - 4 * a * c;
+  let discriminant = b ** 2 - 4 * a * c;
 
   if (discriminant < 0) {
     return []; // нет корней
   } else if (discriminant === 0) {
-    const root = -b / (2 * a);
+    let root = -b / (2 * a);
     return [root]; // один корень
   } else {
-    const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-    const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
     return [root1, root2]; // два корня
   }
 }
