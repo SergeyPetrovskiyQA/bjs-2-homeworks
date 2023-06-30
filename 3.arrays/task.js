1b73ec3dc83e93647587172f73a36fa548400390
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2023-06-30 15:07:02 +0300

    final version

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,26 @@
 function compareArrays(arr1, arr2) {
   
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((value, index) => value === arr2[index]);
 }
 
+
+
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    
+    const filteredUsers = users.filter(user => user.gender === gender);
+    if (filteredUsers.length === 0) {
+      return 0;
+    }
+    const ages = filteredUsers.map(user => user.age);
+    const totalAge = ages.reduce((sum, age) => sum + age, 0);
+    const averageAge = totalAge / ages.length;
+    return averageAge;
+      }
+      
+
+
+
